8c3fdf72727bfee285415d0601d28817712c0c97
rjnares <roberto.nares@gmail.com>
2021-03-31 10:34:47 -0700

    Added express routing to server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,28 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-require("dotenv").config();
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL = "";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
   )
   .catch((error) => console.log(error.message));
 
