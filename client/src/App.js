08e78bc7af01dfc432879447b6ce805d7fc02491
rjnares <roberto.nares@gmail.com>
2021-03-31 11:38:52 -0700

    Added app layout to App.js. Added Posts/Post/Form components to client/src/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Grow from "@material-ui/core/Grow";
+import Grid from "@material-ui/core/Grid";
+
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+// import dejaViewImage from "./images/dejaView.jpg";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static">
+        <Typography variant="h2" align="center">
+          Deja View
+        </Typography>
+        {/*<img src={dejaViewImage} alt="Deja View" height="60" />*/}
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            jsutify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
