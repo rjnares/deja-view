5fc9281f4027df16c3e03b70c7480f3da6a190a7
rjnares <roberto.nares@gmail.com>
2021-04-04 12:44:12 -0700

    Fixed bug where updated post's created time changes after update. Added spacing to like button text

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,9 +28,11 @@ export const updatePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send(`No post with id: ${id}`);
 
-  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
-
-  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { creator, title, message, tags, selectedFile, _id: id },
+    { new: true }
+  );
 
   res.json(updatedPost);
 };
