88126c79e879cd386089889b8f48617efb331a97
rjnares <roberto.nares@gmail.com>
2021-04-05 15:48:10 -0700

    Added new font for app name

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,27 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 import reducers from "./reducers";
-
 import App from "./App";
 import "./index.css";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      fontFamily: ["Pacifico", "cursive"].join(","),
+    },
+  },
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </Provider>,
   document.getElementById("root")
 );
