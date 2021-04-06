bc6a432ef06810001366fe4e7c61d3ea05c125f0
rjnares <roberto.nares@gmail.com>
2021-04-05 19:12:39 -0700

    Split App functionality into Navbar/Home/Auth components. Added routing to App

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,10 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 import dejaViewImage from "../../images/dejaView.png";
 import useStyles from "./styles";
@@ -8,32 +12,70 @@ import useStyles from "./styles";
 const Navbar = () => {
   const classes = useStyles();
 
+  const user = null;
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
-      <div className={classes.headingContainer}>
-        <Typography
-          className={classes.heading}
-          color="primary"
-          variant="h2"
-          align="center"
-        >
-          Deja
-        </Typography>
-        <Typography
-          className={classes.heading}
-          color="secondary"
-          variant="h2"
-          align="center"
-        >
-          View
-        </Typography>
+      <div className={classes.brandContainer}>
+        <div className={classes.headingContainer}>
+          <Typography
+            component={Link}
+            to="/"
+            className={classes.heading}
+            color="primary"
+            variant="h2"
+            align="center"
+          >
+            Deja
+          </Typography>
+          <Typography
+            className={classes.heading}
+            color="secondary"
+            variant="h2"
+            align="center"
+          >
+            View
+          </Typography>
+        </div>
+        <img
+          className={classes.image}
+          src={dejaViewImage}
+          alt="Deja View"
+          height={60}
+        />
       </div>
-      <img
-        className={classes.image}
-        src={dejaViewImage}
-        alt="Deja View"
-        height={60}
-      />
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              className={classes.signout}
+              variant="contained"
+              color="secondary"
+            >
+              Sign Out
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
     </AppBar>
   );
 };
